c96d99f4b6eb852eea11e802587de4c5eb7ab700
Jaryd Krishnan <jaryd@harvestprofit.com>
2020-10-04 20:06:59 -0500

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: '@babel/eslint-parser',
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'react/no-unescaped-entities': 0,
+    'react/style-prop-object': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 0,
+    'function-paren-newline': ['error', 'consistent'],
+    'object-curly-newline': ['error', { consistent: true }],
+    'react/destructuring-assignment': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'no-use-before-define': 0,
+  },
+};
